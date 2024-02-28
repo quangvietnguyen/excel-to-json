9194ae1e44e0f427a6ec312acdd5fdd994d81875
nguye374 <nguye374@sheridancollege.ca>
2024-02-28 01:32:06 -0500

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const ExcelJS = require('exceljs');
+const fs = require('fs');
+
+function main(file) {
+  const list = [];
+  var workbook = new ExcelJS.Workbook();
+  workbook.xlsx
+    .readFile(file)
+    .then(function () {
+      workbook.eachSheet(function (worksheet, sheetId) {
+        worksheet.eachRow(function (row, rowNumber) {
+          rowNumber > 1 &&
+            list.push({ appcode: row.values[1], appname: row.values[2] });
+        });
+      });
+    })
+    .finally(function () {
+      var json = JSON.stringify(list);
+      fs.writeFile(
+        '/Users/vietqnguyen/desktop/test.json',
+        json,
+        'utf8',
+        function (err) {
+          if (err) throw err;
+          console.log('complete');
+        }
+      );
+    });
+}
+
+main('/Users/vietqnguyen/desktop/test.xlsx');
